b88aff7ce2ef5623967660030ef2b16b7960822a
Ben North <ben@redfrontdoor.org>
2022-04-14 21:29:21 +0100

    SeqNum: Define type alias

--- src/websocket-gpios/Types.h
+++ src/websocket-gpios/Types.h
@@ -1,4 +1,10 @@
 #ifndef WEBSOCKET_GPIOS_TYPES_H_
 #define WEBSOCKET_GPIOS_TYPES_H_
 
+#include <cstdint>
+
+////////////////////////////////////////////////////////////////////////////////
+
+using SeqNum = uint64_t;
+
 #endif // WEBSOCKET_GPIOS_TYPES_H_
