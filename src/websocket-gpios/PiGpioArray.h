e420994ab1a8987ce1fd52a76b667e5ebb2148f0
Ben North <ben@redfrontdoor.org>
2022-04-14 22:11:26 +0100

    PiGpioArray: Add skeleton declaration

---
+++ src/websocket-gpios/PiGpioArray.h
@@ -0,0 +1,18 @@
+#ifndef WEBSOCKET_GPIOS_PIGPIOARRAY_H_
+#define WEBSOCKET_GPIOS_PIGPIOARRAY_H_
+
+#include "GpioArray.h"
+
+class PiGpioArray : public IGpioArray
+{
+public:
+    PiGpioArray();
+    virtual ~PiGpioArray();
+
+    Outcome<void> reset() override;
+    Outcome<PinLevel> set_as_input(PinId pin, PullKind pull_kind) override;
+    Outcome<void> set_output(PinId pin, PinLevel level) override;
+    Outcome<void> launch_input_monitor(PinLevelReportFun report_fun) override;
+};
+
+#endif // WEBSOCKET_GPIOS_PIGPIOARRAY_H_
