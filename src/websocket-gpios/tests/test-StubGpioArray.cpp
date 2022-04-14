0fdace18f9ea8beb714b9fd5055f9e99e11cf6e0
Ben North <ben@redfrontdoor.org>
2022-04-14 14:52:11 +0100

    Add "StubGpioArray rejects all methods" test

--- src/websocket-gpios/tests/test-StubGpioArray.cpp
+++ src/websocket-gpios/tests/test-StubGpioArray.cpp
@@ -7,3 +7,27 @@ template <typename T> static void require_failure(const Outcome<T> &outcome)
     REQUIRE(std::holds_alternative<Failure>(outcome));
 }
 
+TEST_CASE("StubGpioArray rejects all methods")
+{
+    StubGpioArray gpios;
+
+    {
+        INFO("reset() should fail");
+        require_failure(gpios.reset());
+    }
+
+    {
+        INFO("set_as_input() should fail");
+        require_failure(gpios.set_as_input(1, PullKind::PULL_DOWN));
+    }
+
+    {
+        INFO("set_output() should fail");
+        require_failure(gpios.set_output(5, 1));
+    }
+
+    {
+        INFO("launch_input_monitor() should fail");
+        require_failure(gpios.launch_input_monitor([](PinId, PinLevel) {}));
+    }
+}
