4e480f5145631a8bbd78868fc1329b10730aea44
Ben North <ben@redfrontdoor.org>
2022-04-14 14:28:47 +0100

    Add "rejects invalid PullKind string" test

--- src/websocket-gpios/tests/test-Types.cpp
+++ src/websocket-gpios/tests/test-Types.cpp
@@ -8,3 +8,11 @@ TEST_CASE("PullKind_from_string parses valid PullKind from string")
     REQUIRE(PullKind_from_string("pull-down") == PullKind::PULL_DOWN);
     REQUIRE(PullKind_from_string("no-pull") == PullKind::NO_PULL);
 }
+
+TEST_CASE("PullKind_from_string rejects invalid PullKind string")
+{
+    REQUIRE_THROWS_MATCHES(
+        PullKind_from_string("banana"),
+        std::invalid_argument,
+        Catch::Matchers::Message("unknown pullKind \"banana\""));
+}
