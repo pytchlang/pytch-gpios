beee8aa255cebeb3a8dfd619965e63384299a424
Ben North <ben@redfrontdoor.org>
2022-04-14 15:03:06 +0100

    PinState_: Add enum

--- src/websocket-gpios/LoopbackGpioArray.h
+++ src/websocket-gpios/LoopbackGpioArray.h
@@ -13,6 +13,16 @@ public:
     Outcome<PinLevel> set_as_input(PinId pin, PullKind pull_kind) override;
     Outcome<void> set_output(PinId pin, PinLevel level) override;
     Outcome<void> launch_input_monitor(PinLevelReportFun report_fun) override;
+
+private:
+    enum class PinState_
+    {
+        INPUT_PU,
+        INPUT_PD,
+        INPUT_NP,
+        DRIVEN_LOW,
+        DRIVEN_HIGH
+    };
 };
 
 #endif // WEBSOCKET_GPIOS_LOOPBACKGPIOARRAY_H_
