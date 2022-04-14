d55bda0934c2667d7ed8c7ebe5f0f77154fcfaad
Ben North <ben@redfrontdoor.org>
2022-04-14 21:52:35 +0100

    InputRecorder: Add test helper

--- src/websocket-gpios/tests/test-loopback.cpp
+++ src/websocket-gpios/tests/test-loopback.cpp
@@ -1,3 +1,17 @@
+#include <mutex>
+
 #include "../LoopbackGpioArray.h"
 
 #include "../vendor/catch2/catch.hpp"
+
+class InputRecorder
+{
+public:
+    void update_pin(PinId pin, PinLevel level);
+    bool pin_eventually_has_level(PinId pin, PinLevel expected_level);
+
+private:
+    using mutex_lock_t = std::lock_guard<std::mutex>;
+    std::unordered_map<PinId, PinLevel> pin_levels_;
+    std::mutex mutex_;
+};
