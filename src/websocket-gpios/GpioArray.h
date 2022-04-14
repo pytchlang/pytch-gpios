74a956e8543abb1ccc06fbd3ddabfb05bbd869ff
Ben North <ben@redfrontdoor.org>
2022-04-14 14:41:27 +0100

    IGPioArray: Define interface

---
+++ src/websocket-gpios/GpioArray.h
@@ -0,0 +1,21 @@
+#ifndef WEBSOCKET_GPIOS_GPIOARRAY_H_
+#define WEBSOCKET_GPIOS_GPIOARRAY_H_
+
+#include <cstdint>
+
+#include "Types.h"
+
+struct IGpioArray
+{
+    virtual ~IGpioArray() {}
+
+    virtual Outcome<void> reset() = 0;
+
+    virtual Outcome<PinLevel> set_as_input(PinId pin, PullKind pull_kind) = 0;
+
+    virtual Outcome<void> set_output(PinId pin, PinLevel level) = 0;
+
+    virtual Outcome<void> launch_input_monitor(PinLevelReportFun report) = 0;
+};
+
+#endif // WEBSOCKET_GPIOS_GPIOARRAY_H_
