57bb160d0c3abae3d0dd49265a9650224d146659
Ben North <ben@redfrontdoor.org>
2022-04-14 14:28:09 +0100

    Add "parses valid PullKind from string" test

--- src/websocket-gpios/tests/test-Types.cpp
+++ src/websocket-gpios/tests/test-Types.cpp
@@ -1,3 +1,10 @@
 #include "../Types.h"
 
 #include "../vendor/catch2/catch.hpp"
+
+TEST_CASE("PullKind_from_string parses valid PullKind from string")
+{
+    REQUIRE(PullKind_from_string("pull-up") == PullKind::PULL_UP);
+    REQUIRE(PullKind_from_string("pull-down") == PullKind::PULL_DOWN);
+    REQUIRE(PullKind_from_string("no-pull") == PullKind::NO_PULL);
+}
