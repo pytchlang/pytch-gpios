2114acbad2cd239afc406a0dc8d343155d11b3ae
Ben North <ben@redfrontdoor.org>
2022-04-14 14:51:50 +0100

    require_failure(): Add test helper

--- src/websocket-gpios/tests/test-StubGpioArray.cpp
+++ src/websocket-gpios/tests/test-StubGpioArray.cpp
@@ -1,3 +1,9 @@
 #include "../StubGpioArray.h"
 
 #include "../vendor/catch2/catch.hpp"
+
+template <typename T> static void require_failure(const Outcome<T> &outcome)
+{
+    REQUIRE(std::holds_alternative<Failure>(outcome));
+}
+
