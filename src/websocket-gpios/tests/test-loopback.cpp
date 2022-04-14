52ba671b8824eefd3052e313c4c3be79de549e04
Ben North <ben@redfrontdoor.org>
2022-04-14 21:52:58 +0100

    pin_eventually_has_level(): Implement

--- src/websocket-gpios/tests/test-loopback.cpp
+++ src/websocket-gpios/tests/test-loopback.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+#include <chrono>
 #include <mutex>
 
 #include "../LoopbackGpioArray.h"
@@ -20,4 +22,26 @@ void InputRecorder::update_pin(PinId pin, PinLevel level)
 {
     mutex_lock_t lock{mutex_};
     pin_levels_.insert_or_assign(pin, level);
+}
+
+bool InputRecorder::pin_eventually_has_level(PinId pin, PinLevel expected_level)
+{
+    bool got_expected = false;
+
+    for (int i = 0; i != 100; ++i)
+    {
+        {
+            mutex_lock_t lock{mutex_};
+
+            if (auto entry = pin_levels_.find(pin);
+                entry != pin_levels_.end() && entry->second == expected_level)
+            {
+                got_expected = true;
+                break;
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    return got_expected;
 }
