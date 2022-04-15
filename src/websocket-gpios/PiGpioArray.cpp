d0abc268c41d4b67c31a15deb9cb51a7264ba11b
Ben North <ben@redfrontdoor.org>
2022-04-15 09:44:34 +0100

    reset(): Implement

--- src/websocket-gpios/PiGpioArray.cpp
+++ src/websocket-gpios/PiGpioArray.cpp
@@ -19,3 +19,36 @@ PiGpioArray::~PiGpioArray()
     // TODO: Any way to report this failing?
     gpioTerminate();
 }
+
+Outcome<void> PiGpioArray::reset()
+{
+    // As per
+    //
+    //   https://www.raspberrypi.org/app/uploads/2012/02/BCM2835-ARM-Peripherals.pdf#page=102
+    //
+    // omitting reserved GPIOs 0 and 1, and only going up to the
+    // maximum GPIO brought out to the header (27).
+    //
+    static std::vector<std::pair<int, int>> power_up_states
+        = {{2, PI_PUD_UP},    {3, PI_PUD_UP},    {4, PI_PUD_UP},
+           {5, PI_PUD_UP},    {6, PI_PUD_UP},    {7, PI_PUD_UP},
+           {8, PI_PUD_UP},    {9, PI_PUD_DOWN},  {10, PI_PUD_DOWN},
+           {11, PI_PUD_DOWN}, {12, PI_PUD_DOWN}, {13, PI_PUD_DOWN},
+           {14, PI_PUD_DOWN}, {15, PI_PUD_DOWN}, {16, PI_PUD_DOWN},
+           {17, PI_PUD_DOWN}, {18, PI_PUD_DOWN}, {19, PI_PUD_DOWN},
+           {20, PI_PUD_DOWN}, {21, PI_PUD_DOWN}, {22, PI_PUD_DOWN},
+           {23, PI_PUD_DOWN}, {24, PI_PUD_DOWN}, {25, PI_PUD_DOWN},
+           {26, PI_PUD_DOWN}, {27, PI_PUD_DOWN}};
+
+    for (const auto &pin_state : power_up_states)
+    {
+        auto outcome = gpioSetMode(pin_state.first, PI_INPUT);
+        if (outcome != 0)
+            return Failure{"failed to set pin to INPUT"};
+        outcome = gpioSetPullUpDown(pin_state.first, pin_state.second);
+        if (outcome != 0)
+            return Failure{"failed to set pull-up/-down of pin"};
+    }
+
+    return Success<void>{};
+}
