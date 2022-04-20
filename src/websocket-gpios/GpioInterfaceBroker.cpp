bc43ebc0c4594823d1d703fdcffb6f832b8d18aa
Ben North <ben@redfrontdoor.org>
2022-04-20 13:56:43 +0100

    maybe_send_pin_level_(): Implement

--- src/websocket-gpios/GpioInterfaceBroker.cpp
+++ src/websocket-gpios/GpioInterfaceBroker.cpp
@@ -3,7 +3,10 @@
 #include <memory>
 
 #include "GpioInterfaceBroker.h"
+#include "GpioJsonInterface.h"
+#include "MessageTransmitChannel.h"
 #include "GpioArray.h"
+#include "Types.h"
 
 using namespace std::placeholders;
 
@@ -14,4 +17,20 @@ GpioInterfaceBroker::GpioInterfaceBroker(std::shared_ptr<IGpioArray> gpios)
 {
     gpios_->launch_input_monitor(
         std::bind(&GpioInterfaceBroker::maybe_send_pin_level_, this, _1, _2));
+}
+
+void GpioInterfaceBroker::maybe_send_pin_level_(PinId pin, PinLevel level)
+{
+    // TODO: We're only reading active_transmit_channel_, so
+    // perhaps we don't need to lock?
+    mutex_lock_t lock{mutex_};
+
+    auto transmit_channel{active_transmit_channel_.lock()};
+    if (transmit_channel)
+    {
+        const auto message
+            = GpioJsonInterface::report_input_message(pin, level);
+
+        transmit_channel->send(std::make_shared<std::string>(message));
+    }
 }
