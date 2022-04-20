e65f873ab60357f4bc4ff3c9a5fd9fa086bbc7e3
Ben North <ben@redfrontdoor.org>
2022-04-20 13:53:10 +0100

    Implement ctor

--- src/websocket-gpios/GpioInterfaceBroker.cpp
+++ src/websocket-gpios/GpioInterfaceBroker.cpp
@@ -1,3 +1,17 @@
+#include <functional>
+#include <utility>
+#include <memory>
+
 #include "GpioInterfaceBroker.h"
+#include "GpioArray.h"
+
+using namespace std::placeholders;
 
 ////////////////////////////////////////////////////////////////////////////////
+
+GpioInterfaceBroker::GpioInterfaceBroker(std::shared_ptr<IGpioArray> gpios)
+    : gpios_(std::move(gpios))
+{
+    gpios_->launch_input_monitor(
+        std::bind(&GpioInterfaceBroker::maybe_send_pin_level_, this, _1, _2));
+}
