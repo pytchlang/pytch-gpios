3f5a96798cc28c2693b0019c3c056b1ea46a3c23
Ben North <ben@redfrontdoor.org>
2022-04-20 15:06:41 +0100

    issue_json_interface(): Test

--- src/websocket-gpios/tests/test-GpioInterfaceBroker.cpp
+++ src/websocket-gpios/tests/test-GpioInterfaceBroker.cpp
@@ -1,13 +1,18 @@
 #include "../GpioInterfaceBroker.h"
 #include "../MessageTransmitChannel.h"
+#include "../GpioJsonInterface.h"
 #include "../LoopbackGpioArray.h"
 
 #include "../vendor/catch2/catch.hpp"
+
+#include "json-requires.h"
 
 #include <memory>
 #include <string>
 #include <vector>
 #include <mutex>
+
+using json = nlohmann::json;
 
 struct RecordingMessageChannel : public IMessageTransmitChannel
 {
@@ -28,6 +33,29 @@ static std::shared_ptr<IGpioArray> gpios;
 
 TEST_CASE("GpioInterfaceBroker")
 {
+    const auto reset_message = R"([{"seqnum": 1234, "kind": "reset"}])";
+
     gpios = std::make_shared<LoopbackGpioArray>();
     GpioInterfaceBroker broker{gpios};
+
+    // The first request to issue a JSON interface should give us
+    // a real interface.
+    auto recorder = std::make_shared<RecordingMessageChannel>();
+    auto json_ifc = broker.issue_json_interface(recorder);
+    auto reply = json_ifc->do_commands(reset_message);
+    JsonRequires::require_sole_ok(json::parse(reply), 1234);
+
+    // A request to issue a second interface should give a stub one.
+    auto json_ifc_1 = broker.issue_json_interface(recorder);
+    reply = json_ifc_1->do_commands(reset_message);
+    JsonRequires::require_sole_error(
+        json::parse(reply), 1234, "already in use");
+
+    // Assigning a fresh shared-ptr to "recorder" should destroy the
+    // existing RecordingMessageChannel, meaning that the broker should
+    // let us get a new non-stub interface.
+    recorder = std::make_shared<RecordingMessageChannel>();
+    json_ifc = broker.issue_json_interface(recorder);
+    reply = json_ifc->do_commands(reset_message);
+    JsonRequires::require_sole_ok(json::parse(reply), 1234);
 }
