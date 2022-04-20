875bd64e4e159acbe853c71ce656447185878881
Ben North <ben@redfrontdoor.org>
2022-04-20 13:51:20 +0100

    GpioInterfaceBroker: Declare public side

--- src/websocket-gpios/GpioInterfaceBroker.h
+++ src/websocket-gpios/GpioInterfaceBroker.h
@@ -1,4 +1,21 @@
 #ifndef WEBSOCKET_GPIOS_GPIOINTERFACEBROKER_H
 #define WEBSOCKET_GPIOS_GPIOINTERFACEBROKER_H
 
+#include <memory>
+
+////////////////////////////////////////////////////////////////////////////////
+
+class IGpioArray;
+class GpioJsonInterface;
+class IMessageTransmitChannel;
+
+class GpioInterfaceBroker
+{
+public:
+    explicit GpioInterfaceBroker(std::shared_ptr<IGpioArray> gpios);
+
+    std::shared_ptr<GpioJsonInterface> issue_json_interface(
+        std::shared_ptr<IMessageTransmitChannel> message_transmit_channel);
+};
+
 #endif // WEBSOCKET_GPIOS_GPIOINTERFACEBROKER_H
