70f6641489563b157bfb798cba4daf306c944001
Ben North <ben@redfrontdoor.org>
2022-04-20 15:26:13 +0100

    WebSocketListener: Add skeleton declaration

---
+++ src/websocket-gpios/WebSocketListener.h
@@ -0,0 +1,12 @@
+#ifndef WEBSOCKET_GPIOS_WEBSOCKETLISTENER_H_
+#define WEBSOCKET_GPIOS_WEBSOCKETLISTENER_H_
+
+#include <memory>
+
+////////////////////////////////////////////////////////////////////////////////
+
+class WebSocketListener : public std::enable_shared_from_this<WebSocketListener>
+{
+};
+
+#endif // WEBSOCKET_GPIOS_WEBSOCKETLISTENER_H_
