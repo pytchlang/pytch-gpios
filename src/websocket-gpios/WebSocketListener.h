4d2d18489cab3130bde3453942254dcdf386bd70
Ben North <ben@redfrontdoor.org>
2022-04-20 15:27:47 +0100

    Declare public interface

--- src/websocket-gpios/WebSocketListener.h
+++ src/websocket-gpios/WebSocketListener.h
@@ -3,10 +3,23 @@
 
 #include <memory>
 
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+
+class GpioInterfaceBroker;
+
 ////////////////////////////////////////////////////////////////////////////////
 
 class WebSocketListener : public std::enable_shared_from_this<WebSocketListener>
 {
+public:
+    WebSocketListener(
+        boost::asio::io_context *ioc,
+        boost::asio::ip::tcp::endpoint endpoint,
+        GpioInterfaceBroker *interface_broker);
+
+    void run();
 };
 
 #endif // WEBSOCKET_GPIOS_WEBSOCKETLISTENER_H_
