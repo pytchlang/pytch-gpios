ac35f5d03ea1e53ccd207e503c4fad7b6580c70f
Ben North <ben@redfrontdoor.org>
2022-04-20 15:01:48 +0100

    RecordingMessageChannel: Add test helper

--- src/websocket-gpios/tests/test-GpioInterfaceBroker.cpp
+++ src/websocket-gpios/tests/test-GpioInterfaceBroker.cpp
@@ -1,6 +1,25 @@
 #include "../GpioInterfaceBroker.h"
+#include "../MessageTransmitChannel.h"
 
 #include "../vendor/catch2/catch.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <mutex>
+
+struct RecordingMessageChannel : public IMessageTransmitChannel
+{
+    void send(const std::shared_ptr<const std::string> message) override
+    {
+        mutex_lock_t lock{mutex_};
+        messages.push_back(*message);
+    }
+
+    using mutex_lock_t = std::lock_guard<std::mutex>;
+    std::mutex mutex_;
+    std::vector<std::string> messages;
+};
 
 TEST_CASE("GpioInterfaceBroker")
 {
