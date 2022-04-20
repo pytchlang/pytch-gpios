822340f551e736849a299e65904c4e5582639bc0
Ben North <ben@redfrontdoor.org>
2022-04-20 15:17:15 +0100

    run(): Implement
    Refers to to-be-defined on_run_() method.

--- src/websocket-gpios/ClientSession.cpp
+++ src/websocket-gpios/ClientSession.cpp
@@ -1,4 +1,6 @@
 #include "ClientSession.h"
+#include "GpioInterfaceBroker.h"
+#include "bind-macro.h"
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
@@ -9,4 +11,14 @@ namespace net = boost::asio;
 ClientSession::ClientSession(net::ip::tcp::socket &&socket)
     : ws_(std::move(socket))
 {
+}
+
+void ClientSession::run(GpioInterfaceBroker *interface_broker)
+{
+    json_interface_
+        = interface_broker->issue_json_interface(shared_from_this());
+
+    // We need to be executing within a strand to perform async
+    // operations on the websocket I/O objects in this session.
+    net::dispatch(ws_.get_executor(), BIND_FRONT_THIS(&ClientSession::on_run_));
 }
