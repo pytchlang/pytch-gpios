97d6994c3cb5386d85a50b6af7ef5ab7b42cfa58
Ben North <ben@redfrontdoor.org>
2022-04-25 12:20:19 +0100

    set_output_message_str(): Add test helper

--- src/websocket-gpios/tests/python/test_websocket.py
+++ src/websocket-gpios/tests/python/test_websocket.py
@@ -9,3 +9,13 @@ def set_input_message_str(seqnum, pin, pull_kind):
         "pullKind": pull_kind,
     }
     return json.dumps([command])
+
+
+def set_output_message_str(seqnum, pin, level):
+    command = {
+        "seqnum": seqnum,
+        "kind": "set-output",
+        "pin": pin,
+        "level": level,
+    }
+    return json.dumps([command])
